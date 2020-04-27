91969c33745fa8a4d44e872bc65a07ad6ec48818
Manvan33 <rikostik@gmail.com>
2020-04-27 15:52:28 +0200

    Profile 0.4
    Bio limitée, couleurs de profil

--- js/lab_fe.js
+++ js/lab_fe.js
@@ -39,6 +39,16 @@ jQuery(function($){
 function LABloadProfile() {
   console.log("loaded by"+this);
   jQuery(function($) {
+    $("#lab_profile_card").css('background-color',$("#lab_profile_card").attr('bg-color'));
+    $("#lab_profile_colorpicker").spectrum({
+      color: "#f2f2f2",
+      move: function(tinycolor) {
+        jQuery("#lab_profile_card").css('background-color',tinycolor);
+      },
+      change: function(tinycolor) {
+        jQuery("#lab_profile_card").attr('bg-color',tinycolor.toHexString());
+      }
+    });
     $(".entry-title").text("Profil de "+$('#lab_profile_name').text().replace("• "," "))
     $("#lab_profile_edit").click( function() {
       $(".lab_profile_edit").show();
@@ -48,24 +58,32 @@ function LABloadProfile() {
     });
 
     $("#lab_confirm_change").click(function(){
-      $(".lab_profile_edit").hide();
-      $("#lab_profile_edit").show();
-      $("#lab_confirm_change").hide();
-      $(".lab_current").show();
-      lab_profile_edit($(this).attr('user_id'), $("#lab_profile_edit_phone").val(), $("#lab_profile_edit_url").val(), $("#lab_profile_edit_bio").val())
+      if ($("#lab_profile_edit_bio").val().length > 200)
+      {
+        $("#lab_alert").html("Votre biographie est trop longue (max 200 caractères).")
+      }
+      else{
+        $(".lab_profile_edit").hide();
+        $("#lab_profile_edit").show();
+        $("#lab_confirm_change").hide();
+        $(".lab_current").show();
+        regex=/\"/g;
+        lab_profile_edit($(this).attr('user_id'), $("#lab_profile_edit_phone").val(), $("#lab_profile_edit_url").val(), $("#lab_profile_edit_bio").val().replace(regex,"”").replace(/\'/g,"’"),jQuery("#lab_profile_card").attr('bg-color'));
+      }
     })
   });
 }
 if ( jQuery( "#lab_profile_card" ).length ) {
   LABloadProfile();
 }
-function lab_profile_edit($user_id,$phone,$url,$bio) {
+function lab_profile_edit($user_id,$phone,$url,$bio,$color) {
   data = {
     'action' : 'lab_profile_edit',
     'phone' : $phone,
     'user_id' : $user_id,
     'url' : $url,
-    'description' : $bio
+    'description' : $bio,
+    'bg_color': $color
   }
   jQuery.post(LAB.ajaxurl, data, function(response) {
     jQuery("#lab_profile_card")[0].outerHTML=response.data;
