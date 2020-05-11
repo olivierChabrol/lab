2290b958a357e318559fafe4f97acc2efc51f891
Astrid <astrid.beyer@etu.univ-amu.fr>
2020-05-11 09:21:19 +0200

    bootstrap script

--- wp-lab.php
+++ wp-lab.php
@@ -72,6 +72,15 @@ if (lab_locale == 'fr_FR') {
 } else {
   define('lab_lang', 'en');
 }
+
+function bootstrap_script() {
+  wp_enqueue_style('bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
+  wp_enqueue_script( 'boot1','https://code.jquery.com/jquery-3.3.1.slim.min.js', array( 'jquery' ),'',true );
+  wp_enqueue_script( 'boot2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array( 'jquery' ),'',true );
+  wp_enqueue_script( 'boot3','https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array( 'jquery' ),'',true );
+}
+add_action( 'wp_enqueue_scripts', 'bootstrap_script' );
+
 /**
  * Load plugin textdomain.
  *
