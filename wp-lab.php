75171d6528aa243ad00ea57769c7ae24d6db16aa
chabrol <olivier.chabrol@univ-amu.fr>
2020-05-11 15:00:50 +0200

    complete new jquery version

--- wp-lab.php
+++ wp-lab.php
@@ -79,7 +79,14 @@ function bootstrap_script() {
   //wp_enqueue_script( 'boot2','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array( 'jquery' ),'',true );
   //wp_enqueue_script( 'boot3','https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array( 'jquery' ),'',true );
 }
-add_action( 'wp_enqueue_scripts', 'bootstrap_script' );
+add_action( 'wp_enqueue_scripts', 'replace_core_jquery_version' );
+
+function replace_core_jquery_version() {
+  wp_deregister_script( 'jquery-core' );
+  wp_enqueue_script('jquery-core', plugins_url('js/jquery-3.5.1.min.js',__FILE__), array(), version_id(), false);
+  wp_deregister_script( 'jquery-migrate' );
+  wp_register_script( 'jquery-migrate', plugins_url("js/jquery-migrate-3.3.0.min.js",__FILE__), array(), '3.3.0',false );
+}
 
 /**
  * Load plugin textdomain.
@@ -201,8 +208,8 @@ function wp_lab_menu()
  **/
 function admin_enqueue()
 {
-  wp_deregister_script('jquery');
-	wp_enqueue_script('jquery', plugins_url('js/jquery-3.5.1.min.js',__FILE__), array(), version_id(), false);
+  //wp_deregister_script('jquery');
+	//wp_enqueue_script('jquery', plugins_url('js/jquery-3.5.1.min.js',__FILE__), array(), version_id(), false);
   //wp_enqueue_script('lab', plugins_url('js/lab_global.js',__FILE__), array('jquery', 'jquery-ui-core','jquery-ui-widget','jquery-ui-position','jquery-ui-sortable','jquery-ui-datepicker','jquery-ui-autocomplete','jquery-ui-dialog'), filemtime(dirname(plugin_basename("__FILE__"))."/js/lab_global.js"), false);
   //wp_register_script('wp-lab', plugins_url('js/lab_global.js',__FILE__), array('jquery', 'jquery-ui-core','jquery-ui-widget','jquery-ui-position','jquery-ui-sortable','jquery-ui-datepicker','jquery-ui-autocomplete','jquery-ui-dialog'), "1.3");
   //Plugin permettant d'afficher les toasts :
@@ -231,8 +238,8 @@ function admin_enqueue()
  */
 function wp_lab_fe_enqueues()
 {
-  wp_deregister_script('jquery');
-	wp_enqueue_script('jquery', plugins_url('js/jquery-3.5.1.min.js',__FILE__), array(), version_id(), false);
+  //wp_deregister_script('jquery');
+	//wp_enqueue_script('jquery', plugins_url('js/jquery-3.5.1.min.js',__FILE__), array(), version_id(), false);
   wp_enqueue_style('jqueryToastCSS',plugins_url('css/jquery.toast.css',__FILE__), version_id(), false);
   wp_enqueue_script('jqueryToastJS',plugins_url('js/jquery.toast.js',__FILE__), array('jquery'),version_id(),false);
   
