c81c45ffc5129ed1c2c9629abb7213ff704309c6
Lucas Urgenti <lurgenti@gmail.com>
2020-05-11 16:46:35 +0200

    Auto stash before merge of "lucas" and "IvanV2"

--- shortcode/lab-shortcode-invitation.php
+++ shortcode/lab-shortcode-invitation.php
@@ -299,6 +299,17 @@ function lab_invitations_interface($args) {
     $listInvitationStr .= lab_invitations_interface_fromList($list,$param['view']);
     $listInvitationStr .=   '</tbody>
                           </table>';
+    $listInvitationStr .=   '<ul id="pagination-digg">
+                                <li class="previous-off">« Précédent</li>
+                                <li class="active">1</li>
+                                <li><a href="/?page=2">2</a></li>
+                                <li><a href="/?page=3">3</a></li>
+                                <li><a href="/?page=4">4</a></li>
+                                <li><a href="/?page=5">5</a></li>
+                                <li><a href="/?page=6">6</a></li>
+                                <li><a href="/?page=7">7</a></li>
+                                <li class="next"><a href="/?page=2">Suivant »</a></li>
+                            </ul><br>';
     if ($param['view']=='admin') {
         $listInvitationStr .=
         '<h2 id="lab_invite_detail_title">'.esc_html__("Détails de l'invitation",'lab').'<i class="fas fa-arrow-up"></i></h2>
