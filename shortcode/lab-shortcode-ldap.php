60a16f175094e476e9206aa3fda2ed40ae322e39
Astrid <astrid.beyer@etu.univ-amu.fr>
2020-05-19 17:06:06 +0200

    début modal edit

--- shortcode/lab-shortcode-ldap.php
+++ shortcode/lab-shortcode-ldap.php
@@ -57,7 +57,7 @@ function lab_ldap($args) {
                         <li><b>'.esc_html("Login","lab").'</b> : '    . (get_ldap_data_from_mail($mail)[2]) . '</li>
                     </ul></div>';
     }
-
+    $ldapStr .= editModal();
     return $ldapStr;
 }
 
@@ -96,8 +96,24 @@ function lab_ldap_pagination($pages, $currentPage) {
  */
 function editModal()
 {
-    $str = '<div id="lab_admin_ldap_edit" class="modal">
-                allo?
-            </div>';
+    $str = '<div class="modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+            </div>
+        </div>';
     return $str;
 }
