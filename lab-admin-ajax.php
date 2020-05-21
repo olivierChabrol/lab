c21ea9defbe6e36ad27f425c04152e2602107e1b
chabrol <olivier.chabrol@univ-amu.fr>
2020-05-21 22:11:41 +0200

    remove ldap url, pass as parameter

--- lab-admin-ajax.php
+++ lab-admin-ajax.php
@@ -1264,7 +1264,7 @@ function lab_ldap_add_user() {
   }
 }
 function lab_ldap_amu_lookup() {
-  $url = "http://ldap.i2m.univ-amu.fr/getAMUUser.php?token=".AdminParams::get_params_fromId(AdminParams::PARAMS_LDAP_TOKEN)[0]->value."&query=".$_POST['query'];
+  $url = "http://".AdminParams::get_params_fromId(AdminParams::PARAMS_LDAP_HOST)[0]->value."/getAMUUser.php?token=".AdminParams::get_params_fromId(AdminParams::PARAMS_LDAP_TOKEN)[0]->value."&query=".$_POST['query'];
   // create curl resource
   $ch = curl_init();
   // set url
