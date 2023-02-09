8bf7db5e8d1f414145629fafb1e25337fdf28a5d
Olivier CHABROL <olivier.chabrol@univ-amu.fr>
2023-02-09 17:17:24 +0100

    year

--- js/lab_fe.js
+++ js/lab_fe.js
@@ -264,7 +264,7 @@ jQuery(function($){
   }
 
   function loadInternship() {
-    $year = $("#lab_intern_selected_year").val();
+    let year = $("#lab_intern_selected_year").val();
     $("#lab_internship_year").val(year);
     let data = {
       'action': 'lab_internship_load',
