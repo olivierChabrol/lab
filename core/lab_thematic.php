e0f260e75d91ab492329d2bce1528976256f6264
Olivier CHABROL <olivier.chabrol@univ-amu.fr>
2024-01-03 14:35:11 +0100

    pour rgdesign

--- core/lab_thematic.php
+++ core/lab_thematic.php
@@ -3,6 +3,8 @@ use AdminParams as AdminParams;
 
 function lab_get_thematic()
 {
+    // pour tester
+    // curl -X POST http://www.i2m.univ-amu.fr/wp-admin/admin-ajax.php?action=lab_thematics -H "Content-Type: application/json"
     $thematics = lab_admin_thematic_load_all();
     global $wpdb;
     $results = $wpdb->get_results($sql);
@@ -28,9 +30,11 @@ function lab_shortcode_thematic_display()
 {
     $thematics = lab_admin_thematic_load_all();
     global $wpdb;
+    $sql = "SELECT DISTINCT thematic_id FROM `".$wpdb->prefix."lab_users_thematic`";
     $results = $wpdb->get_results($sql);
-    $html = "";
+    $html = "<div class='lab_thematics'>";
     foreach($thematics as $thematic) {
+        $html .= "<div class='lab_thematics_row'>";
         $thematic_id = $thematic->id;
         $label = $thematic->value;
         $sql = "SELECT ut.*, umfn.meta_value as firstName, umln.meta_value as lastName, umsn.meta_value as slug FROM `".$wpdb->prefix."lab_users_thematic` AS ut  
@@ -39,16 +43,26 @@ function lab_shortcode_thematic_display()
         LEFT JOIN ".$wpdb->prefix."usermeta as umsn ON ut.user_id=umsn.user_id 
         WHERE umln.meta_key='last_name' AND umfn.meta_key='first_name' AND umsn.meta_key='lab_user_slug' AND ut.thematic_id=".$thematic_id;
         $results = $wpdb->get_results($sql);
-        $html .= "<h3>".$label."</h3>";
+        $html .= "<div class='lab_thematics_row_thematic'>";
+        $html .= $label;
+        $html .= "</div>"; // fin lab_thematics_row_thematic
         foreach($results as $r) 
         {
-            $html .= "- ".$r->firstName." ".strtoupper($r->lastName);//."<br>";
-            $html .= " (";
+            $html .= "<div class='lab_thematics_row_user'>";
+            $html .= "<div class='lab_thematics_row_user_firstname'>";
+            $html .= $r->firstName;
+            $html .= "</div>"; // fin lab_thematics_row_user_firstname
+            $html .= "<div class='lab_thematics_row_user_firstname'>";
+            $html .= strtoupper($r->lastName);
+            $html .= "</div>"; // fin lab_thematics_row_user_firstname
+            $html .= "<div class='lab_thematics_row_user_main'>";
             $html .= $r->main == 1?"P":"S";
-            $html .= ")";
-            $html .= "<br>";
+            $html .= "</div>"; // fin lab_thematics_row_user
+            $html .= "</div>"; // fin lab_thematics_row_user
         }
+        $html .= "</div>"; // fin lab_thematics_row
     }
+    $html .= "</div>"; // fin lab_thematics
     return $html;
 }
 
